cd66cd7133c0eeb6c22d50a7bd679da2c3ab4ae9
andrtolv <andrius.tolvaisis@gmail.com>
2026-01-22 18:47:03 +0200

    Removed deprecated @vscode/webview-ui-toolkit dependency.

--- sigrid-ext-app/src/app/app.config.ts
+++ sigrid-ext-app/src/app/app.config.ts
@@ -4,13 +4,6 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './auth/auth.interceptor';
-import {
-  provideVSCodeDesignSystem,
-  vsCodeButton,
-  vsCodeDataGrid,
-  vsCodeDataGridCell,
-  vsCodeDataGridRow, vsCodePanels, vsCodePanelTab, vsCodePanelView
-} from '@vscode/webview-ui-toolkit';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,13 +12,3 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
-
-provideVSCodeDesignSystem().register(
-  vsCodePanels(),
-  vsCodePanelTab(),
-  vsCodePanelView(),
-  vsCodeButton(),
-  vsCodeDataGrid(),
-  vsCodeDataGridRow(),
-  vsCodeDataGridCell()
-);
