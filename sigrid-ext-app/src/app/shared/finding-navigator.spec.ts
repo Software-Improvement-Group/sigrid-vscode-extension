e770636bd6bab25cc6f3f2a1ead37187933e04de
Andrius Tolvaišis <andrius.tolvaisis@softwareimprovementgroup.com>
2026-02-12 17:51:40 +0200

    Temporary FindingNavigator directive's test fix.

--- sigrid-ext-app/src/app/shared/finding-navigator.spec.ts
+++ sigrid-ext-app/src/app/shared/finding-navigator.spec.ts
@@ -1,8 +1,12 @@
 import { FindingNavigator } from './finding-navigator';
+import {TestBed} from '@angular/core/testing';
 
 describe('FindingNavigator', () => {
+
   it('should create an instance', () => {
-    const directive = new FindingNavigator();
-    expect(directive).toBeTruthy();
+    TestBed.runInInjectionContext(() => {
+      const directive = new FindingNavigator();
+      expect(directive).toBeTruthy();
+    })
   });
 });
